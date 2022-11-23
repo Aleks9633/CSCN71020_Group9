c76da722eabcaf7bd2f224ba15a3e95bbb6e00e8
Aleks <agrujicic9633@conestogac.on.ca>
2022-11-22 19:24:54 -0500

    4 test cases created for 1st function. 3/4 Succesful.

--- Test_analyzeTriangle/Test_analyzeTriangle.cpp
+++ Test_analyzeTriangle/Test_analyzeTriangle.cpp
@@ -1,6 +1,8 @@
 #include "pch.h"
 #include "CppUnitTest.h"
 
+extern "C" char* analyzeTriangle(int side1, int side2, int side3);
+
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace TestanalyzeTriangle
@@ -8,9 +10,29 @@ namespace TestanalyzeTriangle
 	TEST_CLASS(TestanalyzeTriangle)
 	{
 	public:
-		
+
 		TEST_METHOD(TestMethod1)
 		{
+			char* Result = analyzeTriangle(1, 3, 3);
+			Assert::AreEqual("Isosceles triangle", Result);
+		}
+
+		TEST_METHOD(TestMethod2)
+		{
+			char* Result = analyzeTriangle(3, 3, 3);
+			Assert::AreEqual("Equilateral triangle", Result);
+		}
+
+		TEST_METHOD(TestMethod3)
+		{
+			char* Result = analyzeTriangle(5, 7, 8);
+			Assert::AreEqual("Scalene triangle", Result);
+		}
+
+		TEST_METHOD(TestMethod4)
+		{
+			char* Result = analyzeTriangle(1, 2, 3);
+			Assert::AreEqual("it does not form a triangle. invalid input", Result);
 		}
 	};
 }
