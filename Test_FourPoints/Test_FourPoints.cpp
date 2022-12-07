ad1be5bd6c1c6f65e296827260ceed5b59a10de9
Louis He <Lhe5921@conestogac.on.ca>
2022-12-06 22:10:20 -0500

    Fix Test Case

--- Test_FourPoints/Test_FourPoints.cpp
+++ Test_FourPoints/Test_FourPoints.cpp
@@ -16,63 +16,62 @@ namespace TestFourPoints
 		
 		TEST_METHOD(TestMethod1)
 		{
-			int point1x = 0;
-			int point1y = 0;
-			int point2x = 0;
-			int point2y = 4;
-			int point3x = 4;
-			int point3y = 0;
-			int point4x = 4;
-			int point4y = 4;
+			int X1 = 0;
+			int Y1 = 0;
+			int X2 = 0;
+			int Y2 = 4;
+			int X3 = 4;
+			int Y3 = 0;
+			int X4 = 4;
+			int Y4 = 4;
 			char somestring[50] = "";
-			char* result = polygonPoints(point1x, point1y, point2x, point2y, point3x, point3y, point4x, point4y, somestring);
+			char* result = point(X1, Y1, X2, Y2, X3, Y3, X4, Y4, somestring);
 			Assert::AreEqual("16.00,and 16", result);
 
 		}
 		TEST_METHOD(TestMethod2)
 		{
-			int point1x = 4;
-			int point1y = 10;
-			int point2x = 4;
-			int point2y = 12;
-			int point3x = 8;
-			int point3y = 10;
-			int point4x = 8;
-			int point4y = 12;
+			int X1 = 4;
+			int Y1 = 10;
+			int X2 = 4;
+			int Y2 = 12;
+			int X3 = 8;
+			int Y3 = 10;
+			int X4 = 8;
+			int Y4 = 12;
 			char somestring[50] = "";
-			char* result = polygonPoints(point1x, point1y, point2x, point2y, point3x, point3y, point4x, point4y, somestring);
+			char* result = point(X1, Y1, X2, Y2, X3, Y3, X4, Y4, somestring);
 			Assert::AreEqual("8.00,and 12", result);
 
 		}
 		TEST_METHOD(TestMethod3)
 		{
-			int point1x = 1;
-			int point1y = 1;
-			int point2x = 1;
-			int point2y = 1;
-			int point3x = 1;
-			int point3y = 1;
-			int point4x = 1;
-			int point4y = 1;
+			int X1 = 1;
+			int Y1 = 1;
+			int X2 = 1;
+			int Y2 = 1;
+			int X3 = 1;
+			int Y3 = 1;
+			int X4 = 1;
+			int Y4 = 1;
 			char somestring[50] = "";
-			char* result = polygonPoints(point1x, point1y, point2x, point2y, point3x, point3y, point4x, point4y, somestring);
+			char* result = point(X1, Y1, X2, Y2, X3, Y3, X4, Y4, somestring);
 			Assert::AreEqual("This is not a rectangle.\n", result);
 
 		}
 		TEST_METHOD(TestMethod4)
 		{
-			int point1x = 15;
-			int point1y = 7;
-			int point2x = 29;
-			int point2y = 16;
-			int point3x = 0;
-			int point3y = 8;
-			int point4x = 1;
-			int point4y = 1;
+			int X1 = 15;
+			int Y1 = 7;
+			int X2 = 29;
+			int Y2 = 16;
+			int X3 = 0;
+			int Y3 = 8;
+			int X4 = 1;
+			int Y4 = 1;
 			char somestring[50] = "";
-			char* result = polygonPoints(point1x, point1y, point2x, point2y, point3x, point3y, point4x, point4y, somestring);
-			Assert::AreEqual("This is not a rectangle.\n", result);
-
+			char* result = point(X1, Y1, X2, Y2, X3, Y3, X4, Y4, somestring);
+			Assert::AreEqual("Not A Rectangle!\n", result);
 		}
 
 	};
